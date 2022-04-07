ad215adc54650027ee90dd1a3ea36df04735ad17
chandan thakur <chandan7073251686@gmail.com>
2022-04-07 23:38:21 +0530

    Splice

--- Array.js
+++ Array.js
@@ -1499,6 +1499,73 @@ myHonda.color = 'purple';
 console.log('The new color of my Honda is ' + myHonda.color);  //The new color of my Honda is purple
 
 
+/*******************************************************Array.prototype.splice()******************************/
+// The splice() method changes the contents of an array by removing or replacing existing elements and/or 
+// adding new elements in place	. 
+// To access part of an array without modifying it
+/*
+Syntax
+splice(start) 
+splice(start, deleteCount)
+splice(start, deleteCount, item1) 
+splice(start, deleteCount, item1, item2, itemN)
+*/
+
+const months = ['Jan', 'March', 'April', 'June'];
+
+months.splice(1,0,"Feb");
+console.log(months); //['Jan', 'Feb', 'March', 'April', 'June']
+
+months.splice(4,1,"May");
+console.log(months);  //['Jan', 'Feb', 'March', 'April', 'May', 'June']
 
 
+// Remove 0 (zero) elements before index 2, and insert "drum"
+let myFish = ['angel', 'clown', 'mandarin', 'sturgeon'];
+
+let removed = myFish.splice(2,0,"drum");
+console.log(myFish);  // ['angel', 'clown', 'drum', 'mandarin', 'sturgeon']
+
+// Remove 0 (zero) elements before index 2, and insert "drum" and "guitar"
+let myFish1 = ['angel', 'clown', 'mandarin', 'sturgeon'];
+
+let removed1 = myFish1.splice(2,0,"drum","guitar");
+console.log(myFish1);  //['angel', 'clown', 'drum', 'guitar', 'mandarin', 'sturgeon']
+
+
+// Remove 1 element at index 3
+let myFish2 = ['angel', 'clown', 'drum', 'mandarin', 'sturgeon'];
+
+let removed2 = myFish2.splice(3,1);
+console.log(myFish2);  //['angel', 'clown', 'drum', 'sturgeon']
+
+// Remove 1 element at index 2, and insert "trumpet"
+let myFish3 = ['angel', 'clown', 'drum', 'sturgeon'];
+
+let removed3 = myFish3.splice(2,1,"trumpet"); 
+console.log(myFish3);  //['angel', 'clown', 'trumpet', 'sturgeon']
+
+// Remove 2 elements from index 0, and insert "parrot", "anemone" and "blue"
+let myFish4 = ['angel', 'clown', 'trumpet', 'sturgeon'];
+
+let removed4 = myFish4.splice(0,2,"parrot","anemone","blue");
+console.log(myFish4);  // ['parrot', 'anemone', 'blue', 'trumpet', 'sturgeon']
+
+// Remove 2 elements, starting from index 2
+let myFish5 = ['parrot', 'anemone', 'blue', 'trumpet', 'sturgeon'] 
+
+let removed5 = myFish5.splice(2,2);
+console.log(myFish5);  //['parrot', 'anemone', 'sturgeon']
+
+// Remove 1 element from index -2
+let myFish6 = ['angel', 'clown', 'mandarin', 'sturgeon'] ;
+
+let removed6 = myFish6.splice(-2,1);
+console.log(myFish6);  //['angel', 'clown', 'sturgeon']
+
+// Remove all elements, starting from index 2
+let myFish7 = ['angel', 'clown', 'mandarin', 'sturgeon'];
+
+let removed7 = myFish7.splice(2);
+console.log(myFish7); //['angel', 'clown']
 
