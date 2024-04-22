93ff2360a90c90b27c773580084ca4fbf990a1f1
chandan thakur <chandan7073251686@gmail.com>
2024-04-23 00:05:09 +0530

    event 1.1

--- Event/event.js
+++ Event/event.js
@@ -22,13 +22,14 @@
 
 // An example: handling a click event
 
-const btn = document.querySelector("#btn");
+const btn = document.querySelector("#btn");  
 
 function random(number){
     return Math.floor(Math.random() * (number + 1));
 }
 
-btn.addEventListener("click", () => {
+btn.addEventListener("click", (e) => {   
+    console.log(e);
     const color = `rgb(${random(255)},${random(255)},${random(255)})`;
     document.body.style.background = color;
 });
@@ -51,6 +52,176 @@ function changecolor(){
     document.body.style.background = color;
 }
 
-btn2.addEventListener("click", changecolor);
+btn2.addEventListener("click", changecolor); 
 
-// https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Events
+// Removing listeners: 
+// If you've added an event handler using addEventListener(), you can remove it again using the removeEventListener() method. 
+
+
+// For example, this would remove the changeBackground() event handler:
+
+// btn2.removeEventListener("click", changecolor);
+// myElement.removeEventListener("event", functionA)
+
+
+// Adding multiple listerners for a single event
+// myElement.addEventListener("event", functionA);
+// myElement.addEventListener("event", functionB);
+
+// myElement.onclick =  functionA;
+// myElement.onclick = functionB;
+
+
+
+// Event handler properties
+// Objects (such as buttons) that can fire events also usually have properties whose name is on followed by the name of the event.
+
+// For example, elements have a property onclick. This is called an event handler property.
+
+const btn3 = document.querySelector("#btn3"); 
+
+function random(number){
+    return Math.floor(Math.random() * (number + 1));
+}
+
+btn3.onclick = () => {  
+    const color = `rgb(${random(255)},${random(255)},${random(255)})`;
+    document.body.style.backgroundColor = color;
+}
+
+
+// You can also set the handler property to a named function:
+const btn4 = document.querySelector("#btn4"); 
+btn4.onclick = changecolor;  
+
+
+
+// Inline event handler ---- don't use these
+function backgrounColor(){
+    changecolor();
+}
+
+// const allbtn = document.querySelectorAll("button");
+// console.log(allbtn);
+
+// for(const btn of allbtn){
+//     btn.addEventListener("click", changecolor);
+// }
+
+
+// Event Object
+// Sometimes, inside an event handler function, you'll see a parameter specified with a name such as event, evt, or e.
+
+// This is called the event object, and it is automatically passed to event handlers to provide extra features and information.
+
+const btn5 = document.querySelector("#btn5");
+
+btn5.addEventListener("click", (e) => {
+    const color = `rgb(${random(255)} ${random(255)} ${random(255)})`;
+    e.target.style.backgrounColor = color;
+    console.log(e);
+});
+
+// Here you can see we are including an event object, e, in the function, and in the function setting a background color style on e.target — which is the button itself. The target property of the event object is always a reference to the element the event occurred upon. So, in this example, we are setting a random background color on the button, not the page.
+
+
+// Extra properties of event object
+// Most event objects have a standard set of properties and methods available on the event object
+// For example, the keydown event fires when the user presses a key
+
+const inputBox = document.querySelector("#textbox");
+const output = document.querySelector("#output");
+
+inputBox.addEventListener("keydown", (e)  => {
+    output.innerHTML = `You pressed ${e.key}`;
+});
+
+
+
+// Preventing default behavior
+// The most common example is that of a web form, for example, a custom registration form. When you fill in the details and click the submit button, the natural behavior is for the data to be submitted to a specified page on the server for processing, and the browser to be redirected to a "success message" page of some kind (or the same page, if another is not specified).
+
+// The trouble comes when the user has not submitted the data correctly — as a developer, you want to prevent the submission to the server and give an error message saying what's wrong and what needs to be done to put things right.
+
+// Example
+
+const fname = document.querySelector("#fname");
+const lname = document.querySelector("#lname");
+const form = document.querySelector("form");
+const para = document.querySelector("#para");
+
+form.addEventListener("submit", (e) => {
+    if(fname.value === "" || lname.value === ""){
+        e.preventDefault();
+        para.textContent = "You need to fill in both names"
+    }
+});
+
+
+// Now some JavaScript — here we implement a very simple check inside a handler for the submit event (the submit event is fired on a form when it is submitted) that tests whether the text fields are empty. 
+
+// If they are, we call the preventDefault() function on the event object — which stops the form submission — and then display an error message in the paragraph below our form to tell the user what's wrong:
+
+// video player example
+const videoBtn = document.querySelector(".vider_player");
+const hidden = document.querySelector(".hidden");
+const video = document.querySelector(".play");
+
+// videoBtn.addEventListener("click", () => {
+//     hidden.classList.remove("hidden");
+// });
+
+// video.addEventListener("click" , () =>{
+//     video.play();
+// });
+
+// hidden.addEventListener("click", () =>{
+//     hidden.classList.add("hidden");
+// });
+
+// The video is inside the <div> — it is part of it — so clicking the video runs both the event handlers, causing this behavior.
+
+// Fixing the problem with stopPropagation()
+// As we saw in the last section, event bubbling can sometimes create problems, but there is a way to prevent it. The Event object has a function available on it called stopPropagation() which, when called inside an event handler, prevents the event from bubbling up to any other elements.
+
+videoBtn.addEventListener("click", () => {
+    hidden.classList.remove("hidden");
+});
+
+video.addEventListener("click" , (e) =>{
+    e.stopPropagation();
+    video.play();
+});
+
+hidden.addEventListener("click", () =>{
+    hidden.classList.add("hidden");
+});
+
+// All we're doing here is calling stopPropagation() on the event object in the handler for the <video> element's 'click' event. This will stop that event from bubbling up to the box.
+
+
+// Event delegation
+// Example
+function randomD(number){
+    return Math.floor(Math.random() * (number + 1));
+}
+
+function tilesColor(){
+    const bgcolor = `rgb(${randomD(255)} ${randomD(255)} ${randomD(255)})`;
+    return bgcolor;
+}
+
+const container = document.querySelector(".container");
+
+container.addEventListener("click", (event) => {
+    event.target.style.backgroundColor = tilesColor();
+});
+
+// Note: In this example, we're using event.target to get the element that was the target of the event (that is, the innermost element). If we wanted to access the element that handled this event (in this case the container) we could use event.currentTarget.
+
+
+
+
+
+// https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Events 
+// https://www.codeguage.com/courses/js/events-event-objects
